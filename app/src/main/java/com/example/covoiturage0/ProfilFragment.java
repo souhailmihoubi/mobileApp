4e8206fb8fb40c85897e7eeaa670680a09eebc75
souhailmihoubi <souhaiel.mihoubi@nabeul.r-iset.tn>
2022-12-14 01:37:09 +0100

    updated

--- app/src/main/java/com/example/covoiturage0/ProfilFragment.java
+++ app/src/main/java/com/example/covoiturage0/ProfilFragment.java
@@ -1,5 +1,6 @@
 package com.example.covoiturage0;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,6 +20,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfilFragment extends Fragment {
 
+    Button logout;
+    FirebaseAuth mailAuth;
 
     TextView name,email;
     View view;
@@ -29,6 +33,14 @@ public class ProfilFragment extends Fragment {
 
         name = view.findViewById(R.id.name);
         email = view.findViewById(R.id.email);
+        logout = view.findViewById(R.id.logoutbtn);
+
+        mailAuth = FirebaseAuth.getInstance();
+
+        logout.setOnClickListener(view -> {
+            mailAuth.signOut();
+            startActivity(new Intent(getContext(),MainActivity.class));
+        });
 
 
         return view;
